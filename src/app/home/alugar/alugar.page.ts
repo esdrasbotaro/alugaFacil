d17c4b4db7ab149c5cd9324f9c5de65bd33c1bbc
Daniel Victor <souza.dvictor@gmail.com>
2020-10-15 14:58:09 -0300

    Adicionando mais um Imovel

--- src/app/home/alugar/alugar.page.ts
+++ src/app/home/alugar/alugar.page.ts
@@ -17,9 +17,13 @@ export class AlugarPage implements OnInit {
  
   getImoveis() {
     this.imoveis = [
-      new Imovel(1,'Apartamento com 2 Quartos', 295.000, 'Rua Augusta', '400', 'Consolação', 'São Paulo', 
+      new Imovel(1,'Apartamento com 2 Quartos', 3000, 'Rua Augusta', '400', 'Consolação', 'São Paulo', 
       'SP', 'https://s2.glbimg.com/S68qSscRNuEDNZ2vavmrtyUWfA8=/512x320/smart/e.glbimg.com/og/ed/f/original/2018/12/10/apartamento-pequeno-doob-arquitetura09.jpg',
-      '45',2,1,'04905-000')]
+      '45',2,1,'04905-000'),
+      new Imovel(2,'Casa com 3 Quartos', 4000, 'Rua Paim', '287', 'Bela Vista', 'São Paulo', 
+      'SP', 'https://www.queopsimobiliaria.com.br/foto_/2020/2522/barretos-casa-padrao-fortaleza-10-08-2020_12-26-47-0.jpg',
+      '70',3,2,'04570-010')
+    ]
   }}
  
 export class Imovel {
